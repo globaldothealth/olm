0f62bf73a203cfc991aaee6e4cea247fa1ccdd85
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-08-29 22:57:42 +0100

    outbreaks: update for mpox-2024 and marburg fixes

--- src/obr/outbreaks/__init__.py
+++ src/obr/outbreaks/__init__.py
@@ -11,47 +11,70 @@ from ..plots import (
 )
 
 outbreak_marburg = [
-    ("data", get_counts, dict(date_col="Data_up_to")),
+    ("data", get_counts, {"date_col": "Data_up_to"}),
     (
         "figure/epicurve",
         plot_epicurve,
-        dict(
-            title="Date of symptom onset",
-            date_col="Date_onset_estimated",
-            groupby_col="Case_status",
-        ),
+        {
+            "title": "Date of symptom onset",
+            "date_col": "Date_onset_estimated",
+            "groupby_col": "Case_status",
+        },
     ),
     (
         "figure/epicurve_location_status",
         plot_timeseries_location_status,
-        dict(admin_column="Location_District"),
+        {"admin_column": "Location_District"},
     ),
     ("figure/age_gender", plot_age_gender),
     (
         "figure/delay_distribution_consult",
         plot_delay_distribution,
-        dict(
-            col="Date_of_first_consult",
-            title="Delay to consultation from onset",
-            index="A",
-            max_delay_days=20,
-        ),
+        {
+            "col": "Date_of_first_consult",
+            "title": "Delay to consultation from onset",
+            "index": "A",
+            "max_delay_days": 20,
+        },
     ),
     (
         "figure/delay_distribution_death",
         plot_delay_distribution,
-        dict(
-            col="Date_death",
-            title="Delay to death from onset",
-            index="B",
-            max_delay_days=20,
-        ),
+        {
+            "col": "Date_death",
+            "title": "Delay to death from onset",
+            "index": "B",
+            "max_delay_days": 20,
+        },
     ),
 ]
 
 outbreak_mpox_2024 = [
     ("data", get_counts, dict(date_col="Date_entry")),
-    ("figure/epicurve", plot_epicurve),
+    (
+        "figure/epicurve_source_report",
+        plot_epicurve,
+        {
+            "title": "Date of report in primary source",
+            "date_col": "Source_I_Date report",
+            "groupby_col": "Case_status",
+        },
+    ),
+    (
+        "figure/epicurve_confirmed",
+        plot_epicurve,
+        {
+            "title": "Date of case confirmation",
+            "date_col": "Date_confirmation",
+            "groupby_col": "Case_status",
+            "values": ["confirmed"],
+        },
+    ),
+    ("figure/age_gender", plot_age_gender),
 ]
-OUTBREAKS = {"marburg": outbreak_marburg, "mpox-2024": outbreak_mpox_2024}
+
+OUTBREAKS = {
+    "marburg": {"plots": outbreak_marburg, "additional_date_columns": ["Data_up_to"]},
+    "mpox-2024": {"plots": outbreak_mpox_2024},
+}
 __all__ = ["OUTBREAKS"]
